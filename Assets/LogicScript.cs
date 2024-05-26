532e3d02b303154b6ca4c8032a604733dbeaf63f
ThatsNotMe <mineflax@gmail.com>
2024-05-26 11:33:44 +0200

    LogicScript, ...
    Added Logic Script to handle Score, GameOver and Restart

---
+++ Assets/LogicScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour {
+    public int playerScore;
+    public Text ScoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd) {
+        playerScore += scoreToAdd;
+        ScoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() {
+        gameOverScreen.SetActive(true);
+    }
+}
